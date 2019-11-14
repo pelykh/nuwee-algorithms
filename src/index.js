4c935b71e03b9524efe3ff08acaa96ae928f4623
Andrew Pelykh <andrew.pelykh@gmail.com>
2019-11-14 20:07:09 +0200

    Move aco demo to separate file

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-import Colony from './algorithms/aco/Colony';
-import Graph from './graph';
+import acoDemo from './algorithms/aco/demo';
 
-const graph = new Graph('canvas');
-const counter = document.getElementById('counter');
-
-document.getElementById('start-button')
-  .addEventListener('click', () => {
-    graph.unsubscribe();
-    counter.style.display = 'block';
-    const distances = graph.edges;
-    const popSize = document.getElementById('pop-size-input').value || 20;
-    const maxIterations = document.getElementById('iterations-input').value || 200;
-    const alpha = document.getElementById('alpha-input').value || 1;
-    const beta = document.getElementById('beta-input').value || 2;
-    const pho = document.getElementById('pho-input').value || 0.1;
-    const ip = document.getElementById('ip-input').value || 1;
-    const q = document.getElementById('q-input').value || 1;
-
-    const colony = new Colony(popSize, maxIterations, distances, alpha, beta, pho, ip, q);
-    colony.initialise();
-    colony.setOnIteration((x, pheromones) => {
-      counter.innerHTML = `Running ${x}`;
-      graph.render(pheromones);
-    });
-    colony.setOnNewBest((x) => {
-        counter.innerHTML = `Complete ${x}`;
-    });
-
-    colony.iterate();
-  });
+acoDemo('app');
