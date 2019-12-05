464c50c1709bcd0c3281094d85e2cc3b38c46268
Andrew Pelykh <andrew.pelykh@gmail.com>
2019-12-05 18:49:54 +0200

    Implement backpropagation algorithm

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import acoDemo from './algorithms/aco/demo';
 import geneticDemo from './algorithms/genetic_algorithm/demo';
 import markovDemo from './algorithms/markov_chain/demo';
 import art1Demo from './algorithms/art1/demo';
+import backPropagationDemo from './algorithms/backpropagation/demo';
 import gameOfLifeDemo from './algorithms/game_of_life/demo';
 
 // acoDemo('app');
 // art1Demo('app');
 // geneticDemo('app');
 // markovDemo('app');
-gameOfLifeDemo('app');
+// gameOfLifeDemo('app');
+backPropagationDemo('app');
